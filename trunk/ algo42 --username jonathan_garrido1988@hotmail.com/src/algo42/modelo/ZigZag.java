aa6c1853e774457bb76f6d12ff54d6d67a041c47
aeeisnerg <aeeisnerg@6ecfaf77-45b4-aa15-d9f5-7067a6377cc4>
2011-05-31 18:31:32 +0000

    Escribi algunos de los métodos que me tocan...

--- trunk/ algo42 --username jonathan_garrido1988@hotmail.com/src/algo42/modelo/ZigZag.java
+++ trunk/ algo42 --username jonathan_garrido1988@hotmail.com/src/algo42/modelo/ZigZag.java
@@ -1,5 +1,37 @@
+import java.lang.Math.*;
 package algo42.modelo;
 
 public class ZigZag extends EstrategiaArmados {
-
+	
+	private int indicadorDeSentido;
+	private int periodoDeVueloHaciaUnLado;
+	private Punto posicion;
+	private Nave nave;
+	private Mision mision;
+	
+	private void actualizarDatosDeMovimiento(){
+		//actualizo la cantidad de movimientos restantes
+		//hacia el lado que se esta moviendo
+		periodoDeVueloHaciaUnLado = periodoDeVueloHaciaUnLado - 1;
+		
+		//actualizo el director de dicho movimiento
+		if (periodoDeVueloHaciaUnLado = 0){
+			indicadorDeSentido = indicadorDeSentido * -1;
+			//el valor del perriodoDeVueloHaciaUnLado es arbitrario...
+			periodoDeVueloHaciaUnLado = 3;
+		}
+	}
+	
+	public void mover(){
+		float longitudDeMovimiento = 1/sqrt(2);
+		
+		float movimiento_x = posicion.getX() + longitudDeMovimiento;
+		float movimiento_y = posicion.getY() + longitudDeMovimiento * indicadorDeSentido;
+		
+		posicion.setX( movimiento_x );
+		posicion.setY( movimiento_y );
+		
+		this.actualizarDatosDeMovimiento();
+	}
+		
 }
